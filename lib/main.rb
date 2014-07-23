368d589ee0685cfa425c156b761c8ad0a0a76a05
Andreas Siegemund (smumm) <andreas.siegemund@gmx.net>
2014-07-23 11:31:29 +0200

    Remove class Arguments from module TeaTime

--- lib/main.rb
+++ lib/main.rb
@@ -1,17 +1,17 @@
 require_relative "teatime"
-require_relative "teatime/arguments"
+require_relative "arguments"
 
-args = TeaTime::Arguments.new(ARGV)
+args = Arguments.new(ARGV)
 
 filename = "C:/Users/siegemund/Times.txt"
 
-if (args.command == TeaTime::Arguments::LIST)
+if (args.command == Arguments::LIST)
   TeaTime.list(filename)
-elsif (args.command == TeaTime::Arguments::STOP)
+elsif (args.command == Arguments::STOP)
   TeaTime.stop(filename)
-elsif (args.command == TeaTime::Arguments::NEW)
+elsif (args.command == Arguments::NEW)
   TeaTime.new(filename, args.taskname)
-elsif (args.command == TeaTime::Arguments::PRINT)
+elsif (args.command == Arguments::PRINT)
   TeaTime.print(filename)
 else
   puts "Invalid argument(s) #{ARGV}"
