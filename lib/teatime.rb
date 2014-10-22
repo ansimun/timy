9aea5a1bf708904c40ee3691eb1ea85bae2930a8
Andreas Siegemund <hausensausen@gmail.com>
2014-10-22 17:35:29 +0200

    Replace class timetable with class tracker

--- lib/teatime.rb
+++ lib/teatime.rb
@@ -1,50 +1,39 @@
-require_relative "teatime/timetable"
+require_relative "teatime/tracker"
 require_relative "teatime/textreport"
-require_relative "teatime"
 
 module TeaTime
-    def self.read(filename)
-      return TimeTable.new(IO.read(filename))
-    end
-    
-    def self.write(filename, timetable)
-      IO.write(filename, timetable.to_csv)
-    end
-    
     def self.new(filename, taskname)
-      times = read(filename)
-      times.last.stop unless times.last.nil?
-      task = TeaTime::Task.new(taskname, DateTime.now)
-      times.add(task)
-      write(filename, times)
+      tracker = Tracker.new.read(filename)
+      tracker.new_task(taskname)
+      tracker.write(filename)
     end
     
     def self.stop(filename)
-      times = read(filename)
-      times.last.stop unless times.last.nil?
-      write(filename, times)
+      tracker = Tracker.new.read(filename)
+      tracker.stop_task
+      tracker.write(filename)
     end
     
     def self.list(filename)
-      times = read(filename)
-      times.each do |task|
+      tracker = Tracker.new.read(filename)
+      tracker.each_task do |task|
         puts "#{task.name}"
         puts "\t#{task.start_time} - #{task.end_time} : #{task.elapsed_hours.round(2)} hours"
       end
     end
     
     def self.print(filename, expression)
-      times = read(filename)
-      print_last_month(times, expression)
-      print_this_month(times, expression)
-      print_today(times, expression)
+      tracker = Tracker.new.read(filename)
+      print_last_month(tracker, expression)
+      print_this_month(tracker, expression)
+      print_today(tracker, expression)
     end
     
-    def self.print_today(times, expression)
+    def self.print_today(tracker, expression)
       year_day = DateTime.now.yday
       report = TeaTime::TextReport.new("Today")
-      report.last_task = times.last
-      times.each do |task|
+      report.last_task = tracker.last_task
+      tracker.each_task do |task|
         if (task.start_time.yday == year_day && /#{expression}/i === task.name)
           report.add_task(task)
         end
@@ -52,11 +41,11 @@ module TeaTime
       report.print
     end
     
-    def self.print_this_month(times, expression)
+    def self.print_this_month(tracker, expression)
       date = DateTime.now
       report = TeaTime::TextReport.new(date.strftime("%B %Y"))
-      report.last_task = times.last
-      times.each do |task|
+      report.last_task = tracker.last_task
+      tracker.each_task do |task|
         if (task.start_time.month == date.month && /#{expression}/i === task.name)
           report.add_task(task)
         end
@@ -64,11 +53,11 @@ module TeaTime
       report.print
     end
     
-    def self.print_last_month(times, expression)
+    def self.print_last_month(tracker, expression)
       date = DateTime.now.prev_month
       report = TeaTime::TextReport.new(date.strftime("%B %Y"))
-      report.last_task = times.last
-      times.each do |task|
+      report.last_task = tracker.last_task
+      tracker.each_task do |task|
         if (task.start_time.month == date.month && /#{expression}/i === task.name)
           report.add_task(task)
         end
---
+++ lib/teatime/tracker.rb
@@ -0,0 +1,67 @@
+require "csv"
+require_relative "task"
+
+module TeaTime
+  class Tracker
+    @tasks
+    
+    def initialize()
+      @tasks = Array.new
+    end
+    
+    def read(filename)
+      @tasks.clear
+      CSV.parse(IO.read(filename), :col_sep => ";") do |row|
+        @tasks.push(read_task_row(row))
+      end
+      return self
+    end
+    
+    def write(filename)
+      csv_table = CSV.generate(:col_sep => ";") do |csv|
+        @tasks.each do |task|
+          csv << [task.name, build_time_string(task.start_time), build_time_string(task.end_time)]
+        end
+      end
+      IO.write(filename, csv_table)
+      return nil
+    end
+    
+    def new_task(taskname)
+      @tasks.last.stop unless @tasks.last.nil?
+      @tasks.push(Task.new(taskname, DateTime.now))
+      return self
+    end
+    
+    def stop_task()
+      @tasks.last.stop unless @tasks.last.nil?
+      return self
+    end
+    
+    def last_task()
+      return @tasks.last.clone unless @tasks.last.nil?
+      return nil
+    end
+    
+    def each_task()
+      @tasks.each do |task|
+        yield task.clone #prevent modifications for given task
+      end      
+      
+      return nil
+    end
+    
+    private
+    
+    def read_task_row(row)
+      raise "Invalid data count in csv row - should be 3 but is #{row.count}" unless row.count == 3
+      result = Task.new(row[0], row[1], row[2])
+      return result;
+    end
+    
+    def build_time_string(time)
+      return (time.nil?)? nil : time.to_s
+    end
+    
+  end
+end
