4ee67dec90aa1b1036f9bab315c6348d73b68c36
Andreas Siegemund <hausensausen@gmail.com>
2014-11-03 12:33:18 +0100

    Ignore case while searching for task to start

--- lib/teatime/tracker.rb
+++ lib/teatime/tracker.rb
@@ -28,7 +28,7 @@ module TeaTime
     end
     
     def start_task(taskname)
-      matching_task = @tasks.find_all { |task|  /#{taskname}/ === task.name}.last
+      matching_task = @tasks.find_all { |task|  /#{taskname}/i === task.name}.last
       if (matching_task != nil)
         new_task(matching_task.name)
       end
