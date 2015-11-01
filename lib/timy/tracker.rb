02f1180255829bfcbe3107a6bf2f85761fe8dd62
Andreas Siegemund <hausensausen@gmail.com>
2015-11-01 15:13:46 +0100

    Replace default license header with LGPL license header

--- lib/timy/tracker.rb
+++ lib/timy/tracker.rb
@@ -1,3 +1,19 @@
+# 
+#    Copyright (C) 2015 Andreas Siegemund (smumm)
+#    
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or any later version.
+#
+#    This library is WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library.
+#    
+
 require "csv"
 require_relative "task"
 
--- test/task_test.rb
+++ test/task_test.rb
@@ -1,6 +1,18 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+# 
+#    Copyright (C) 2015 Andreas Siegemund (smumm)
+#    
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or any later version.
+#
+#    This library is WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library.
+#    
 
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
